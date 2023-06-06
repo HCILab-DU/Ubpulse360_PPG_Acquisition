061fc7b885ff07369fb948c64b86f82a33049fee
Assloper <pghmin@gmail.com>
2023-06-06 14:55:39 +0900

    upload
    PPG_sirial_upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Connect = new System.Windows.Forms.Button();
+            this.textBox_ViewData = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox_Port = new System.Windows.Forms.ComboBox();
+            this.button_Refresh = new System.Windows.Forms.Button();
+            this.button_Disconnect = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_Connect
+            // 
+            this.button_Connect.Location = new System.Drawing.Point(297, 9);
+            this.button_Connect.Name = "button_Connect";
+            this.button_Connect.Size = new System.Drawing.Size(75, 23);
+            this.button_Connect.TabIndex = 0;
+            this.button_Connect.Text = "연결";
+            this.button_Connect.UseVisualStyleBackColor = true;
+            this.button_Connect.Click += new System.EventHandler(this.button_Connect_Click);
+            // 
+            // textBox_ViewData
+            // 
+            this.textBox_ViewData.Location = new System.Drawing.Point(14, 38);
+            this.textBox_ViewData.Multiline = true;
+            this.textBox_ViewData.Name = "textBox_ViewData";
+            this.textBox_ViewData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_ViewData.Size = new System.Drawing.Size(439, 315);
+            this.textBox_ViewData.TabIndex = 1;
+            this.textBox_ViewData.TextChanged += new System.EventHandler(this.textBox_ViewData_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "포트선택";
+            // 
+            // comboBox_Port
+            // 
+            this.comboBox_Port.FormattingEnabled = true;
+            this.comboBox_Port.Location = new System.Drawing.Point(75, 11);
+            this.comboBox_Port.Name = "comboBox_Port";
+            this.comboBox_Port.Size = new System.Drawing.Size(121, 20);
+            this.comboBox_Port.TabIndex = 3;
+            // 
+            // button_Refresh
+            // 
+            this.button_Refresh.Location = new System.Drawing.Point(202, 9);
+            this.button_Refresh.Name = "button_Refresh";
+            this.button_Refresh.Size = new System.Drawing.Size(75, 23);
+            this.button_Refresh.TabIndex = 4;
+            this.button_Refresh.Text = "포트검색";
+            this.button_Refresh.UseVisualStyleBackColor = true;
+            this.button_Refresh.Click += new System.EventHandler(this.button_Refresh_Click);
+            // 
+            // button_Disconnect
+            // 
+            this.button_Disconnect.Location = new System.Drawing.Point(378, 9);
+            this.button_Disconnect.Name = "button_Disconnect";
+            this.button_Disconnect.Size = new System.Drawing.Size(75, 23);
+            this.button_Disconnect.TabIndex = 5;
+            this.button_Disconnect.Text = "해제";
+            this.button_Disconnect.UseVisualStyleBackColor = true;
+            this.button_Disconnect.Click += new System.EventHandler(this.button_Disconnect_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 369);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 55);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "저장";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 618);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_Disconnect);
+            this.Controls.Add(this.button_Refresh);
+            this.Controls.Add(this.comboBox_Port);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_ViewData);
+            this.Controls.Add(this.button_Connect);
+            this.Name = "Form1";
+            this.Text = "UBPulse360 Serial Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Connect;
+        private System.Windows.Forms.TextBox textBox_ViewData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox_Port;
+        private System.Windows.Forms.Button button_Refresh;
+        private System.Windows.Forms.Button button_Disconnect;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 // 네임스페이스 WindowsFormsAPP1로 정의
+{
+    // 공개된 여러파일에 나눠 작성되는 'Form1' 클래스 정의
+    public partial class Form1 : Form 
+    {
+        // 정적으로 선언된 Serialport 변수에 SerialPort 겍체를 생성하여 할당
+        static SerialPort serialPort = new SerialPort();
+
+        public Form1()
+        {
+            InitializeComponent(); // 폼 초기화
+            port_Refresh(); //포트 갱신 및 초기화
+            CheckForIllegalCrossThreadCalls = false; //잘못된 스레드 호출 감지
+        }
+
+        bool Sync_After = false;
+        // 바이트 선언
+        byte Packet_TX_Index = 0;
+        byte Data_Prev = 0;
+        byte PUD0 = 0;
+        byte CRD_PUD2_PCDT = 0;
+        byte PUD1 = 0;
+        byte PacketCount = 0;
+        byte PacketCyclicData = 0;
+        byte psd_idx = 0;
+        static int Ch_Num = 6;
+        static int Sample_Num = 1;
+        byte[] PacketStreamData = new byte[Ch_Num * 2 * Sample_Num];
+
+
+        int Parsing_LXSDFT2(byte data_crnt)
+        {
+            int retv = 0;
+            // 만약 Data_Prev 의 값이 0xFF이고 data_crnt 값이 0xFE인경우
+            if (Data_Prev == 0xFF && data_crnt == 0xFE)   // Message start point
+            {
+                // Sync_After를 True로 설정
+                Sync_After = true;
+                // Packet_TX_Index 를 0으로 설정
+                Packet_TX_Index = 0;
+            }
+
+            // Data_Prev 값을 data_crnt로 갱신한다.
+            Data_Prev = data_crnt;  // Previous data
+            // 만약 Sync_After이 True일 경우
+            if (Sync_After == true)
+            {
+                // Packet_TX_Index를 증가시킨다.
+                Packet_TX_Index++;  // 0xFE: index 1
+                // 만약 Packet_TX_Index 값이 1보다 큰 경우
+                if (Packet_TX_Index > 1)    // Do from index 2
+                {
+                    // Packet_TX_Index가 2인경우 PUD0 변수에 data_crnt값을 저장
+                    if (Packet_TX_Index == 2)       // PUD_0
+                    {
+                        PUD0 = data_crnt;
+                    }
+                    // 만약 Packet_TX_Index가 3인 경우 CRD_PUD2_PCDT 변수에 data_crnt값을 저장
+                    else if (Packet_TX_Index == 3)  // CRD, PUD2, PCDT
+                    {
+                        CRD_PUD2_PCDT = data_crnt;
+                    }
+                    // 만약 Packet_TX_Index가 4인 경우 PacketCount 변수에 data_crnt값을 저장
+                    else if (Packet_TX_Index == 4)  // PC
+                    {
+                        PacketCount = data_crnt;
+                    }
+                    // 만약 Packet_TX_Index가 5인 경우 PUD1 변수에 data_crnt 값을 저장
+                    else if (Packet_TX_Index == 5)  // PUD_1
+                    {
+                        PUD1 = data_crnt;
+                    }
+                    // 만약 Packet_TX_Index가 6일 경우 PacketCyclicData 값을 data_crnt에 저장
+                    else if (Packet_TX_Index == 6)  // PCD
+                    {
+                        PacketCyclicData = data_crnt;
+                    }
+                    // 만약 Packet_TX_Index가 6을 초과할 경우 
+                    else if (Packet_TX_Index > 6)   // PSDs
+                    {
+                        psd_idx = (byte)(Packet_TX_Index - 7);  // PSD index
+                        PacketStreamData[psd_idx] = data_crnt;
+                        if (Packet_TX_Index == (Ch_Num * 2 * Sample_Num + 6)) // Complete
+                        {
+                            Sync_After = false;
+                            retv = 1;
+                        }
+                    }
+                }
+            }
+            return retv;
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int receivedNumber = serialPort.BytesToRead;
+
+            if (receivedNumber > 0)
+            {
+                byte[] buffer = new byte[receivedNumber];
+                serialPort.Read(buffer, 0, receivedNumber);
+
+                foreach (byte receivedData in buffer)
+                {
+                    if (Parsing_LXSDFT2(receivedData) == 1)
+                    {
+                        textBox_ViewData.AppendText(
+                            string.Format("{0:D2}: {1} ms | ",
+                                PacketCount,
+                                ((PUD1 % 0x07) << 8) + PUD0)
+                        );
+                        for (int i = 0; i < Ch_Num; i++)
+                        {
+                            textBox_ViewData.AppendText(string.Format("{0} ", ((PacketStreamData[i * 2] & 0x0F) << 8) + PacketStreamData[i * 2 + 1]));
+                        }
+                        textBox_ViewData.AppendText("\r\n");
+                        /*if (PacketCount >= 31)
+                        {
+                            textBox_ViewData.AppendText("\r\n\r\n");
+                        }*/
+                    }
+                }
+            }
+        }
+
+        private void button_Connect_Click(object sender, EventArgs e)
+        {
+            if (comboBox_Port.Text.Length == 0)
+            {
+                MessageBox.Show("포트를 선택해주세요.");
+                return;
+            }
+
+            if (!serialPort.IsOpen)
+            {
+                serialPort.PortName = comboBox_Port.SelectedItem.ToString();
+                serialPort.BaudRate = 115200;
+                serialPort.DataBits = 8;
+                serialPort.StopBits = StopBits.One;
+                serialPort.Parity = Parity.None;
+                serialPort.DataReceived += new SerialDataReceivedEventHandler(serialPort1_DataReceived);
+                serialPort.Open();
+            }
+        }
+
+        private void port_Refresh()
+        {
+            string[] PortNames = SerialPort.GetPortNames();
+
+            comboBox_Port.Items.Clear();
+            foreach (string portnumber in PortNames)
+            {
+                comboBox_Port.Items.Add(portnumber);
+            }
+        }
+
+        private void button_Refresh_Click(object sender, EventArgs e)
+        {
+            port_Refresh();
+        }
+
+        private void button_Disconnect_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+
+        private void textBox_ViewData_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ()
+            {
+                Sample_Num = 
+            }
+
+        }
+    }
+}
